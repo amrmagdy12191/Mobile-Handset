da7dfa73eaf4410294a006c288579b8b28151522
AmrMagdy <a.magdyabdelaal@gmail.com>
2020-09-18 14:13:23 +0400

    change handset url

--- MobileInfo/src/main/java/com/mobile/utils/Constants.java
+++ MobileInfo/src/main/java/com/mobile/utils/Constants.java
@@ -2,7 +2,7 @@ package com.mobile.utils;
 
 public class Constants {
 	
-	public final static String MOBILE_HANDSET_URL= "http://localhost:8080/DBAPI";
+	public final static String MOBILE_HANDSET_URL= "https://a511e938-a640-4868-939e-6eef06127ca1.mock.pstmn.io/handsets/list";
 	public final static String HTTP_GET = "GET";
 	public final static String HTTP_POST = "POST";
 	public final static String HTTP_PUT = "PUT";
